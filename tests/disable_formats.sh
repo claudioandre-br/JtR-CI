ed8e2e39e5d42bf4805527185d13bd5ac63335ee
Claudio André <claudioandre.br@gmail.com>
2019-06-06 23:32:06 -0300

    tests: fix a bug in 'john-local.conf'

--- tests/disable_formats.sh
+++ tests/disable_formats.sh
@@ -44,8 +44,8 @@ echo 'enpass = Y' >> ../run/john-local.conf      #BE
 
 echo 'RACF-KDFAES = Y' >> ../run/john-local.conf             #SLOW
 echo 'RAR = Y' >> ../run/john-local.conf                     #SLOW
-echo 'wpapsk-opencl' >> ../run/john-local.conf               #SLOW
-echo 'wpapsk-pmk-opencl' >> ../run/john-local.conf           #SLOW
+echo 'wpapsk-opencl = Y' >> ../run/john-local.conf           #SLOW
+echo 'wpapsk-pmk-opencl = Y' >> ../run/john-local.conf       #SLOW
 
 echo 'bcrypt = Y' >> ../run/john-local.conf                  # TS
 echo 'bcrypt-opencl = Y' >> ../run/john-local.conf           # TS
