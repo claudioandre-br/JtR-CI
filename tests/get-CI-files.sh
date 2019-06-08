099a3d4de649c0662b8babbc3c0ab3a22a8a56cd
Claudio André <claudioandre.br@gmail.com>
2019-06-07 22:28:29 -0300

    maint: update 'get-CI-files.sh' to get latest files
    Point to the latest GitLab and AppVeyor builds.

--- tests/get-CI-files.sh
+++ tests/get-CI-files.sh
@@ -20,24 +20,25 @@
 # Disable for '-dev': wget https://launchpad.net/~claudioandre.br/+snap/john-the-ripper/+build/557705/+files/buildlog_snap_ubuntu_xenial_s390x_john-the-ripper_BUILDING.txt.gz
 
 # AppVeyor (Windows 64 and 32 bits)
-wget https://ci.appveyor.com/api/buildjobs/fh0dicj2387oa40c/artifacts/win_x64.7z     -O winX64.7z
-wget https://ci.appveyor.com/api/buildjobs/fh0dicj2387oa40c/artifacts/optional.7z    -O winX64_optional.7z
-wget https://ci.appveyor.com/api/buildjobs/fh0dicj2387oa40c/log                      -O winX64_buildlog.txt
+wget https://ci.appveyor.com/api/buildjobs/6xi3fnryax6hkvk9/artifacts/win_x64.7z     -O winX64_1_JtR.7z
+wget https://ci.appveyor.com/api/buildjobs/6xi3fnryax6hkvk9/artifacts/optional.7z    -O winX64_3_optional.7z
+wget https://ci.appveyor.com/api/buildjobs/6xi3fnryax6hkvk9/log                      -O winX64_2_buildlog.txt
 
 # Disable for '-dev': wget https://ci.appveyor.com/api/buildjobs/dq1hp9jbkmyo0sc4/artifacts/win_x32.zip     -O winX32.zip
 # Disable for '-dev': wget https://ci.appveyor.com/api/buildjobs/dq1hp9jbkmyo0sc4/artifacts/optional.zip    -O winX32_optional.zip
 # Disable for '-dev': wget https://ci.appveyor.com/api/buildjobs/dq1hp9jbkmyo0sc4/log                       -O winX32_buildlog.txt
 
 # GitLab (Linux Flatpak app)
-wget https://gitlab.com/claudioandre-br/JtR-CI/-/jobs/223481577/artifacts/download  -O bundle_JtR.zip
-wget https://gitlab.com/claudioandre-br/JtR-CI/-/jobs/223481577/raw                 -O bundle_buildlog.txt
-wget https://gitlab.com/claudioandre-br/JtR-CI/-/jobs/223481578/raw                 -O bundle_testlog.txt
+wget https://gitlab.com/claudioandre-br/JtR-CI/-/jobs/227581967/artifacts/download  -O bundle_1_JtR.zip
+wget https://gitlab.com/claudioandre-br/JtR-CI/-/jobs/227581967/raw                 -O bundle_2_buildlog.txt
+wget https://gitlab.com/claudioandre-br/JtR-CI/-/jobs/227581974/raw                 -O bundle_3_testlog.txt
 
 # GitHub (Linux Docker image)
 # Disable for '-dev': wget https://api.travis-ci.org/v3/job/532480502/log.txt                               -O docker_buildlog.txt
 
-unzip bundle_JtR.zip
+unzip bundle_1_JtR.zip
 sha256sum *.zip
+sha256sum *.7z
 sha256sum john.flatpak
 
 # Keep only the zipped file
