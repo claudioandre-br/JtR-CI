92e110ec6fd1c1e0bc1a594bdb4c9f4c9fa49caa
Claudio André <claudioandre.br@gmail.com>
2019-11-01 15:57:42 -0300

    maint: refactor 'get-CI-files.sh' to make a release easier
    In order to allow me to make a release changing only a couple of jobs IDs.

--- tests/get-CI-files.sh
+++ tests/get-CI-files.sh
@@ -10,31 +10,51 @@
 # http://www.gnu.org/licenses/gpl-2.0.html
 ######################################################################
 
-# Launchpad (Linux Snap app, various archs)
-# Disable for '-dev': wget https://launchpad.net/~claudioandre.br/+snap/john-the-ripper/+build/557702/+files/buildlog_snap_ubuntu_xenial_armhf_john-the-ripper_BUILDING.txt.gz
-# Disable for '-dev': wget https://launchpad.net/~claudioandre.br/+snap/john-the-ripper/+build/557699/+files/buildlog_snap_ubuntu_xenial_i386_john-the-ripper_BUILDING.txt.gz
-# Disable for '-dev': wget https://launchpad.net/~claudioandre.br/+snap/john-the-ripper/+build/557703/+files/buildlog_snap_ubuntu_xenial_arm64_john-the-ripper_BUILDING.txt.gz
-# Disable for '-dev': wget https://launchpad.net/~claudioandre.br/+snap/john-the-ripper/+build/557701/+files/buildlog_snap_ubuntu_xenial_amd64_john-the-ripper_BUILDING.txt.gz
-# Disable for '-dev': wget https://launchpad.net/~claudioandre.br/+snap/john-the-ripper/+build/557704/+files/buildlog_snap_ubuntu_xenial_ppc64el_john-the-ripper_BUILDING.txt.gz
-# Disable for '-dev': wget https://launchpad.net/~claudioandre.br/+snap/john-the-ripper/+build/557700/+files/buildlog_snap_ubuntu_xenial_powerpc_john-the-ripper_BUILDING.txt.gz
-# Disable for '-dev': wget https://launchpad.net/~claudioandre.br/+snap/john-the-ripper/+build/557705/+files/buildlog_snap_ubuntu_xenial_s390x_john-the-ripper_BUILDING.txt.gz
+# Directory names and folders
+APPVEYOR_64bits="jwpduv9tek0hh7bf"
+APPVEYOR_32bits="" # Disable for '-dev' releases
+FLATPAK="339563801"
+AZURE_ID="177"
 
-# AppVeyor (Windows 64 and 32 bits)
-wget https://ci.appveyor.com/api/buildjobs/jwpduv9tek0hh7bf/artifacts/win_x64.7z     -O winX64_1_JtR.7z
-wget https://ci.appveyor.com/api/buildjobs/jwpduv9tek0hh7bf/log                      -O winX64_2_buildlog.txt
+# AppVeyor (Windows 64 and 32 bits) ############################################
+wget https://ci.appveyor.com/api/buildjobs/$APPVEYOR_64bits/artifacts/win_x64.7z      -O winX64_1_JtR.7z
+wget https://ci.appveyor.com/api/buildjobs/$APPVEYOR_64bits/log                       -O winX64_2_buildlog.txt
+
+if [[ -n "$APPVEYOR_32bits"  ]]; then
+    wget https://ci.appveyor.com/api/buildjobs/$APPVEYOR_32bits/artifacts/win_x32.7z  -O winX32_1_JtR.7z
+    wget https://ci.appveyor.com/api/buildjobs/$APPVEYOR_32bits/log                   -O winX32_2_buildlog.txt
+fi
+
+# GitLab (Linux Flatpak app) ###################################################
+wget https://gitlab.com/claudioandre-br/JtR-CI/-/jobs/$FLATPAK/artifacts/download     -O flatpak_1_JtR.zip
+wget https://gitlab.com/claudioandre-br/JtR-CI/-/jobs/$FLATPAK/raw                    -O flatpak_2_buildlog.txt
+
+    # Azure Windows package
+    wget https://dev.azure.com/claudioandre-br/40224313-b91e-465d-852b-fc4ea516f33e/_apis/build/builds/$AZURE_ID/logs/132 -O azure_buildlog.txt
+
+if [[ "$1" == "LOG_FILES"  ]]; then
+    # Download log files, to commit them in the git repo
+
+    # Launchpad (Linux Snap app, various archs)
+    wget https://launchpad.net/~claudioandre.br/+snap/john-the-ripper/+build/557702/+files/buildlog_snap_ubuntu_xenial_armhf_john-the-ripper_BUILDING.txt.gz
+    wget https://launchpad.net/~claudioandre.br/+snap/john-the-ripper/+build/557699/+files/buildlog_snap_ubuntu_xenial_i386_john-the-ripper_BUILDING.txt.gz
+    wget https://launchpad.net/~claudioandre.br/+snap/john-the-ripper/+build/557703/+files/buildlog_snap_ubuntu_xenial_arm64_john-the-ripper_BUILDING.txt.gz
+    wget https://launchpad.net/~claudioandre.br/+snap/john-the-ripper/+build/557701/+files/buildlog_snap_ubuntu_xenial_amd64_john-the-ripper_BUILDING.txt.gz
+    wget https://launchpad.net/~claudioandre.br/+snap/john-the-ripper/+build/557704/+files/buildlog_snap_ubuntu_xenial_ppc64el_john-the-ripper_BUILDING.txt.gz
+    wget https://launchpad.net/~claudioandre.br/+snap/john-the-ripper/+build/557700/+files/buildlog_snap_ubuntu_xenial_powerpc_john-the-ripper_BUILDING.txt.gz
+    wget https://launchpad.net/~claudioandre.br/+snap/john-the-ripper/+build/557705/+files/buildlog_snap_ubuntu_xenial_s390x_john-the-ripper_BUILDING.txt.gz
+
+    # GitLab (Linux Flatpak app)
+    wget https://gitlab.com/claudioandre-br/JtR-CI/-/jobs/339563802/raw         -O flatpak_3_testlog.txt
+
+    # GitHub (Linux Docker image)
+    wget https://api.travis-ci.org/v3/job/605181618/log.txt                     -O docker_buildlog.txt
+fi
+
+# Deprecated ###################################################################
 #wget https://ci.appveyor.com/api/buildjobs/6xi3fnryax6hkvk9/artifacts/optional.7z    -O winX64_3_optional.7z
-
-# Disable for '-dev': wget https://ci.appveyor.com/api/buildjobs/dq1hp9jbkmyo0sc4/artifacts/win_x32.zip     -O winX32.zip
-# Disable for '-dev': #wget https://ci.appveyor.com/api/buildjobs/dq1hp9jbkmyo0sc4/artifacts/optional.zip    -O winX32_optional.zip
-# Disable for '-dev': wget https://ci.appveyor.com/api/buildjobs/dq1hp9jbkmyo0sc4/log                       -O winX32_buildlog.txt
-
-# GitLab (Linux Flatpak app)
-wget https://gitlab.com/claudioandre-br/JtR-CI/-/jobs/336763895/artifacts/download  -O flatpak_1_JtR.zip
-wget https://gitlab.com/claudioandre-br/JtR-CI/-/jobs/336763895/raw                 -O flatpak_2_buildlog.txt
-#wget https://gitlab.com/claudioandre-br/JtR-CI/-/jobs/227581974/raw                 -O flatpak_3_testlog.txt
-
-# GitHub (Linux Docker image)
-# Disable for '-dev': wget https://api.travis-ci.org/v3/job/532480502/log.txt                               -O docker_buildlog.txt
+#wget https://ci.appveyor.com/api/buildjobs/dq1hp9jbkmyo0sc4/artifacts/optional.zip   -O winX32_optional.zip
+################################################################################
 
 unzip flatpak_1_JtR.zip
 sha256sum *.zip
