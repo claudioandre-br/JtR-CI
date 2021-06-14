fcaaf8be923cdf7223f02cb9adf5cdbad231bbfa
Claudio André <claudioandre.br@gmail.com>
2021-06-14 12:14:27 -0300

    maint: update 'get-CI-files.sh' to get latest files
    Point to the latest GitLab and Windows builds.

--- tests/get-CI-files.sh
+++ tests/get-CI-files.sh
@@ -15,8 +15,8 @@ APPVEYOR_64bits="" # Disabled: I'm using Azure packages
 APPVEYOR_32bits="" # Disabled for '-dev' releases
 
 # Flatpak build IDs
-FLATPAK="1303211846"
-FLATPAK_TEST="1303211850"
+FLATPAK="1344606322"
+FLATPAK_TEST="1344606323"
 # Get pipeline 297118338 info https://gitlab.com/api/v4/projects/12573246/pipelines/297118338
 # Get jobs info               https://gitlab.com/api/v4/projects/12573246/pipelines/297118338/jobs
 
