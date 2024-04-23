fc7c49e6b12f992047d1bb1645d1711ea19597ba
Claudio André <dev@claudioandre.slmail.me>
2024-04-23 13:08:06 -0300

    CI: use a new Docker registry as images source

--- tests/travis-ci.sh
+++ tests/travis-ci.sh
@@ -220,14 +220,14 @@ elif [[ "$TEST" == *"ztex;"* ]]; then
     do_Build_Docker_Command_Image
 
     # Run docker
-    docker run --cap-add SYS_PTRACE -v "$HOME":/root -v "$(pwd)":/cwd ghcr.io/claudioandre-br/john-ci:ubuntu.rolling sh -c "$docker_command"
+    docker run --cap-add SYS_PTRACE -v "$HOME":/root -v "$(pwd)":/cwd ghcr.io/openwall/john-ci:ubuntu.rolling sh -c "$docker_command"
 
 elif [[ "$TEST" == *"fresh;"* ]]; then
     # Build the docker command line
     do_Build_Docker_Command_Image
 
     # Run docker
-    docker run --cap-add SYS_PTRACE --privileged -v "$HOME":/root -v "$(pwd)":/cwd ghcr.io/claudioandre-br/john-ci:ubuntu.devel sh -c "$docker_command"
+    docker run --cap-add SYS_PTRACE --privileged -v "$HOME":/root -v "$(pwd)":/cwd ghcr.io/openwall/john-ci:ubuntu.devel sh -c "$docker_command"
 
 elif [[ "$TEST" == *"experimental;"* ]]; then
     # Build the docker command line
@@ -242,7 +242,7 @@ elif [[ "$TEST" == *"OpenCL;"* ]]; then
     do_Build_Docker_Command_Image
 
     # Run docker
-    docker run --cap-add SYS_PTRACE -v "$HOME":/root -v "$(pwd)":/cwd ghcr.io/claudioandre-br/john-ci:ubuntu.opencl sh -c "$docker_command"
+    docker run --cap-add SYS_PTRACE -v "$HOME":/root -v "$(pwd)":/cwd ghcr.io/openwall/john-ci:ubuntu.opencl sh -c "$docker_command"
 
 elif [[ "$TEST" == *"centos7;"* ]]; then
     # Stable environment (compiler/OS)
@@ -250,7 +250,7 @@ elif [[ "$TEST" == *"centos7;"* ]]; then
     do_Build_Docker_Command_Image
 
     # Run docker
-    docker run -v "$HOME":/root -v "$(pwd)":/cwd ghcr.io/claudioandre-br/john-ci:centos.7 sh -c "$docker_command"
+    docker run -v "$HOME":/root -v "$(pwd)":/cwd ghcr.io/openwall/john-ci:centos.7 sh -c "$docker_command"
 
 elif [[ "$TEST" == *"snap;"* ]]; then
     # Prepare environment
