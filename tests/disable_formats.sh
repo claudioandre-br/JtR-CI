e179a12ce252d52de8e2c6dcc1ddc432a2b5cce6
Claudio André <dev@claudioandre.slmail.me>
2024-11-05 18:36:31 -0300

    CI: enable various formats
    Let's start testing them again.
    
    Signed-off-by: Claudio André <dev@claudioandre.slmail.me>

--- tests/disable_formats.sh
+++ tests/disable_formats.sh
@@ -28,36 +28,18 @@
 # Disable clueless formats
 echo '[Local:Disabled:Formats]' >> ../run/john-local.conf
 
-echo 'pbkdf2-hmac-md4 = Y' >> ../run/john-local.conf
-echo 'pbkdf2-hmac-md5 = Y' >> ../run/john-local.conf
-echo 'OpenBSD-SoftRAID = Y' >> ../run/john-local.conf
-echo 'dpapimk = Y' >> ../run/john-local.conf
-echo 'iwork = Y' >> ../run/john-local.conf
-echo 'ethereum = Y' >> ../run/john-local.conf
-echo 'dmg = Y' >> ../run/john-local.conf
-echo 'adxcrypt = Y' >> ../run/john-local.conf
-echo 'encfs = Y' >> ../run/john-local.conf
-
 echo 'RACF-KDFAES = Y' >> ../run/john-local.conf             #SLOW
 echo 'RAR = Y' >> ../run/john-local.conf                     #SLOW
 echo 'wpapsk-opencl = Y' >> ../run/john-local.conf           #SLOW
 echo 'wpapsk-pmk-opencl = Y' >> ../run/john-local.conf       #SLOW
 
-echo 'pbkdf2-hmac-md4-opencl = Y' >> ../run/john-local.conf  # TS
-echo 'pbkdf2-hmac-md5-opencl = Y' >> ../run/john-local.conf  # TS
-
-echo 'ssh-opencl = Y' >> ../run/john-local.conf  # TS, after 1a06dc4deeca5064e690f89724eb3a05469fd162
-
-echo 'bf-opencl = Y' >> ../run/john-local.conf
+# Let's say these are fragile
 echo 'krb5pa-md5-opencl = Y' >> ../run/john-local.conf
-echo 'mscash2-opencl = Y' >> ../run/john-local.conf
 echo 'o5logon-opencl = Y' >> ../run/john-local.conf
-echo 'raw-SHA512-free-opencl = Y' >> ../run/john-local.conf  # Inefficient
-echo 'xsha512-free-opencl = Y' >> ../run/john-local.conf     # Inefficient
 echo 'mscash-opencl = Y' >> ../run/john-local.conf
 echo 'salted_sha-opencl = Y' >> ../run/john-local.conf
+
 echo 'bitlocker-opencl = Y' >> ../run/john-local.conf # Very slow format
-echo 'keepass-opencl = Y' >> ../run/john-local.conf
 echo 'pgpdisk-opencl = Y' >> ../run/john-local.conf #FAILED (cmp_all(49)) Intel OpenCL CPU
 
 # Formats failing Intel OpenCL CPU driver
@@ -82,22 +64,3 @@ fi
 echo 'streebog256crypt-opencl = Y' >> ../run/john-local.conf
 echo 'streebog512crypt-opencl = Y' >> ../run/john-local.conf
 echo 'gost94crypt-opencl = Y' >> ../run/john-local.conf
-
-#########################################################################
-# TS OpenCL
-# OpenCL descrypt builds for all 4096 salts, it is unusable inside CI
-# rm -rf opencl_rar_fmt_plug.c racf_fmt_plug.c opencl_wpapsk_fmt_plug.c \
-#        opencl_mscash_fmt_plug.c opencl_mscash2_fmt_plug.c \
-#        opencl_rawsha512_fmt_plug.c opencl_xsha512_fmt_plug.c \
-#        opencl_DES_fmt_plug.c opencl_DES_bs_plug.c \
-#        opencl_DES_bs_b_plug.c opencl_DES_bs_f_plug.c opencl_DES_bs_h_plug.c \
-#        opencl_krb5pa-md5_fmt_plug.c
-
-# # TS --internal
-# rm -rf adxcrypt_fmt_plug.c pbkdf2-hmac-md4_fmt_plug.c \
-#        pbkdf2-hmac-md5_fmt_plug.c phpassMD5_fmt_plug.c opencl_keepass_fmt_plug.c \
-#        opencl_pbkdf2_hmac_md4_fmt_plug.c opencl_pbkdf2_hmac_md5_fmt_plug.c \
-#        opencl_bitlocker_fmt_plug.c opencl_bf_fmt_plug.c opencl_o5logon_fmt_plug.c \
-#        opencl_rawsha1_fmt_plug.c opencl_rawmd4_fmt_plug.c opencl_rawmd5_fmt_plug.c \
-#        opencl_mysqlsha1_fmt_plug.c
-# #########################################################################
